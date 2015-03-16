562d792367d40c48f1bfd059db385c160a184b15
Deagels <joakim.hagen@lyse.net>
2015-03-16 22:01:59 +0100

    more batch capability

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Paxos consensus algorithm implemented in the functional validation language Dafn
 ### Installing for Windows
 Get the [latest version of Dafny](https://dafny.codeplex.com/releases/view/135602). The compiler does not require a setup process and you may simply extract the zip somewhere befitting. If you have [Visual Studio](https://www.visualstudio.com/) 2010 or newer installed you may also install the Dafny Language Extension by opening the file *DafnyLanguageService.vsix*
 
+### Compiling
+Open paxos.dfy with *Dafny.exe* or *dfy-compile.bat*. If *dfy-compile.bat* can't find *Dafny.exe*, run it once without arguments where it can find it and add it to the %PATH% system variable. After this, *dfy-compile.bat* can be used from anywhere.
+
 **Notes:** Dafny runs in a .NET environment. Make sure you have [.NET Framework 4](http://www.microsoft.com/net) installed
--- dfy-compile.bat
+++ dfy-compile.bat
@@ -1,24 +1,43 @@
 @echo off
 
-for %%X in (dafny) do (set FOUND=%%~$PATH:X)
+set D=Dafny.exe
+
+for %%X in (%D%) do (set FOUND=%%~$PATH:X)
 if defined FOUND (
   if "%1" == "" goto empty
-  dafny %1
-  echo %~n1.dll
-  goto end
+  goto runcompiler
 )
-:nodafny
-echo Can't find dafny in %%PATH%%
-echo You can get Dafny at http://dafny.codeplex.com/
-echo Press any key to open page in your browser
-pause >nul
+echo Can't find %D% in %%PATH%%
 
-:download
-start /MIN http://dafny.codeplex.com/
-exit
+:search
+for /f %%X in ('dir %D% /b /s') do (
+  set dafnydir=%%~dpX
+  goto found
+)
+
+:nodafny
+echo or in current folder. If you have dafny, run this script in the dafny folder to optionally add it to system %%PATH%% and/or compile successfully.
+echo You can get Dafny at the following link:
+echo.
+echo https://dafny.codeplex.com/releases/view/135602
+goto end
+
+:found
+if not "%1" == "" goto runcompiler
+echo Press any key to add %dafnydir% to %%PATH%% system variable.
+pause >nul
+setx PATH %PATH%;%dafnydir% /m
+echo Done
+goto end
 
 :empty
-echo Usage: Call me with a dfy file as argument or drag-and-drop a file on my icon
+echo Usage: Call me with a dfy file as argument or drag-and-drop a file on my icon.
+echo To add %D% to %%PATH%%, run without arguments from a folder where %D%
+echo can be found.
+goto end
+
+:runcompiler
+call %D% %1
 
 :end
 echo Press any key to exit . . .
